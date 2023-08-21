8a748ae9d99bdffcf9bd6008b99d598b6804e698
Dominik Flüchter <dominik.fluechter@googlemail.com>
2023-08-21 09:03:47 +0200

    add

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+from sklearn import datasets
 st.title("Modelisation")
 
 st.write("""
@@ -9,4 +9,18 @@ Which one is the best?
 
 dataset_name = st.sidebar.selectbox("Select Dataset", ("Iris", "Breast Cancer", "Wine"))
 
+classifier_name = st.sidebar.selectbox("Select Classsifier", ("KNN", "SVM", "Logistic Regression"))
+
+def get_dataset(dataset_name):
+    if dataset_name == "Iris":
+        data = datasets.load_iris()
+    elif dataset_name == "Wine":
+        data = datasets.load_wine()
+    else:
+        data = datasets.load_breast_cancer()
+    X = data.data
+    y = data.target
+    return X, y
+
+X, y = get_dataset(dataset_name)
 st.write(dataset_name) 
